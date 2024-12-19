e52f799c26806340f605edd9aa93162b6838e17f
Mertcanroc <143512927+Mertcanroc@users.noreply.github.com>
2024-12-19 12:12:36 +0100

    dsd

--- les15/OOPHP/Program.php
+++ les15/OOPHP/Program.php
@@ -2,3 +2,12 @@
 include 'opgave.php';
 $umut = new Persoon("Umut", 18, "M");
 $demirel = new Persoon("Demirel", 23, "M");
+$demirel->setLeeftijd(24);
+$age=$demirel->getLeeftijd();
+echo "<br> leeftijd van Demirel is: ", $age; 
+echo "<br>";
+$gegevens1=$demirel->getGegevens();
+$gegevens2=$umut->getGegevens();
+echo $gegevens1;
+echo $gegevens2;
+echo $demirel;
--- les15/OOPHP/opgave.php
+++ les15/OOPHP/opgave.php
@@ -1,34 +1,57 @@
 <?php
+class Persoon
 {
-    class Persoon
-    {
-        public $naam;
-        private $leeftijd;
-        protected $geslacht;
-        public $isStudent;
-        public $gemiddeldCijfer;
- 
+    public $naam;
+    private $leeftijd;
+    protected $geslacht;
+    public $isStudent;
+    public $gemiddeldCijfer;
+
+    function setLeeftijd(int $leeftijd) {
+        $this->leeftijd = $leeftijd;
+    }
+
+    function getLeeftijd() {
+        return $this->leeftijd;
+    }
+
+    function getGegevens() {
+        $gegevens = 
+        "<br> De gegevens van " . $this->naam . " zijn: " . 
+        "<br> Leeftijd: " . $this->leeftijd . 
+        "<br> Geslacht: " . $this->geslacht;
+        return $gegevens;
+    }
+
+    // Corrected the method name to __toString()
+    function __toString() {
+        return("<br>De gegevens van " . $this->naam . " zijn:" .
+        "<br>Leeftijd: " .  $this->leeftijd . 
+        "<br>Geslacht: " . $this->geslacht);
+    }
+
     //constructor methode
     function __construct(string $naam, int $leeftijd, string $geslacht)
     {
-    $this->naam = $naam;
-    $this->leeftijd = $leeftijd;
-    $this->geslacht = $geslacht;
-    echo "<br> Nieuwe persson-object aangemaakt";
-    echo "<br> De property naam is " . $this->naam;
-}
+        $this->naam = $naam;
+        $this->leeftijd = $leeftijd;
+        $this->geslacht = $geslacht;
+        echo "<br> Nieuwe persson-object aangemaakt";
+        echo "<br> De property naam is " . $this->naam;
+    }
+
     function __destruct()
     {
         echo "<br> Persoon object $this->naam wordt verwijderd";
     }
+
     function setGeslacht(string $geslacht)
     {
         $this->geslacht = $geslacht;
     }
+
     function getGeslacht() 
     {
         return $this->geslacht; 
     }
-    
-}
 }
